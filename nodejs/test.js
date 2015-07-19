198c730fe01ba3382e9b3201b8c758194802a327
Billy Zhong <billyczhong@gmail.com>
2015-07-19 16:56:37 -0400

    Reformatting file system

--- nodejs/test.js
+++ nodejs/test.js
@@ -4,7 +4,7 @@ var app = express();
 app.get('/', function(req, res){
 	res.sendFile('/home/cakepop/Cakepop/nodejs/test.html');
 });
-app.get('/src/*', function(req, res){
+app.get('/source/*', function(req, res){
 	res.sendFile('/home/cakepop/Cakepop' + req.path);
 });
 
