e99dcd11662e67151c99968fa747eea74a0d297b
Billy Zhong <billyczhong@gmail.com>
2015-07-19 16:29:12 -0400

    Setting up landing

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('*', function(req, res){
+app.get('/', function(req, res){
 	res.sendFile('/home/cakepop/Cakepop/nodejs/test.html');
+});
+app.get('/assets/*', function(req, res){
+	res.sendFile('/home/cakepop/Cakepop' + req.path);
 });
 
 app.listen(80);
