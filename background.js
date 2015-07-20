28605d0413302d700af9f3353d3517142ca89e1d
Kushal-C <kcutari@gmail.com>
2015-07-20 03:48:10 -0700

    Background process that should run node application

---
+++ background.js
@@ -0,0 +1,12 @@
+
+//Fires off the functions after all the content is loaded, shoud load index.js within socket
+document.addEventListener("DOMContentLoaded",function(){
+	
+
+
+});
+
+//adds the footer with links to the bottom of the page
+function appendFooter(){
+
+}
